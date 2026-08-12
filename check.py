3a040a4fc9e96bc2f4b8d588531e1e41f990cedc
woaizheta <17826976659@163.com>
2026-08-12 10:42:50 +0800

    chore: remove debug file

--- check.py
+++
@@ -1,7 +0,0 @@
-from nongzi.database import SessionLocal
-from nongzi.models.product import Product
-db = SessionLocal()
-a = db.query(Product).filter(Product.is_active == True).count()
-i = db.query(Product).filter(Product.is_active == False).count()
-print(f"active={a} inactive={i}")
-db.close()
